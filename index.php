2d0e3fda435dafae8eabb0c9073fb08d608d5331
qwseops <40327168+qwseops@users.noreply.github.com>
2023-02-12 06:05:26 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Java Burn, ​
+                        Limited Time Special Pricing - Act Now!, ​
+                    Secure Your Reserved Java Burn While Stocks Last, What People Who Have Already Used Java Burn Say, FAQs, ​LIMITED TIME OFFER!">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>CheapKeys</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="CheapKeys.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.8.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": ""
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="CheapKeys">
+    <meta property="og:type" content="website">
+  </head>
+  <body data-home-page="CheapKeys.html" data-home-page-title="CheapKeys" class="u-body u-xl-mode"><header class="u-clearfix u-header u-header" id="sec-06b3"><div class="u-clearfix u-sheet u-valign-middle-xl u-valign-middle-xs u-sheet-1">
+        <a href="https://cheapkeys.online/shop/" class="u-black u-border-2 u-border-hover-palette-1-base u-border-white u-btn u-btn-round u-button-style u-hover-palette-1-base u-radius-50 u-btn-1">CLICK HERE TO BUY</a>
+        <h3 class="u-text u-text-default u-text-1"><b>100% MONEY BACK&nbsp;GUARANTEE</b>
+        </h3>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" style="padding: 9px 2px; font-size: calc(1em + 18px);">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="CheapKeys.html" style="padding: 10px 20px;">CheapKeys</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Terms.html" style="padding: 10px 20px;">Terms</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Terms.html" style="padding: 10px 8px 10px 20px;">Terms</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="CheapKeys.html">CheapKeys</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms.html">Terms</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms.html">Terms</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="sec-8ab8">
+      <div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-container-style u-gradient u-layout-cell u-left-cell u-size-60 u-size-xs-60 u-layout-cell-1" src="">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-align-center u-text u-text-1">
+                    <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-1" href="https://exipure.thebudget.shop/product/tea-burn-keto-diet-pillsweight-lossfat-burnappetite-control-supplement/">#1 ORIGINAL WINDOWS PRODUCT KEYS</a>
+                  </h2>
+                  <p class="u-align-center u-text u-text-body-alt-color u-text-2"><b></b>Buy <b>Windows</b> at the cheapest price in the <b>online market!</b>
+                  </p>
+                  <p class="u-align-center u-text u-text-body-alt-color u-text-3"><b></b>Purchase the <b>key now</b> &amp; keep forever. The easy &amp; affordable way to purchase <b>Microsoft Office</b> for your computer. <b>Variety of Products. Great Customer Support. Discount Office Software.</b><b>Quick, Easy Download.</b>
+                    <br>
+                  </p>
+                  <a href="https://cheapkeys.online/shop/" class="u-border-2 u-border-hover-palette-1-base u-border-white u-btn u-btn-round u-button-style u-hover-palette-1-base u-none u-radius-50 u-btn-2">CLICK HERE TO BUY</a>
+                  <p class="u-align-center u-text u-text-body-alt-color u-text-4"><b> Begin your best software shopping 
+experience here with us. Browse through our bestsellers:&nbsp;<br>Windows 11/10 
+Pro, Office 2019, Office 365 and more. </b>
+                  </p>
+                  <p class="u-align-center u-text u-text-body-alt-color u-text-5"><b> Experience the latest windows with newest technology available on the market.</b>
+                  </p>
+                  <p class="u-align-center u-text u-text-body-alt-color u-text-6"><b> Your satisfaction is our company’s policy.
+Instant delivery, lowest prices, satisfaction guaranteed.</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-image u-section-2" id="sec-b875" data-image-width="1920" data-image-height="1080">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-align-left u-text u-text-1">
+          <span style="font-weight: 700;" class="u-text-white">
+            <br><b>1. Are the license/product keys original? </b>
+            <br>Yes, they are. All license keys are from Microsoft. <br>
+            <br><b>2. How long does the license last? </b>
+            <br>After activation, the license will last a lifetime (including Office 365). <br>
+            <br><b>3. Will I be able to update it? </b>
+            <br>Yes, you will be able to update it for as long as Microsoft provides software support. <br>
+            <br><b>4. How will the license key be delivered? </b>
+            <br>License key(s) will be delivered via email. <br>
+            <br><b>5. How long will the delivery take? </b>
+            <br>Most orders will be delivered almost instantly (within an hour). A 24-hour delivery is guaranteed. <br>
+            <br><b>6. What happens when the provided key does not work? </b>
+            <br>Send us an email of proof that the provided key does not work. After verification, we will take the necessary steps to resolve the issue. If the issue still persists, you will be refunded. We have an easy-refund policy. <br>
+            <br><b>7. What about warranty and support? </b>
+            <br>Any necessary support will provided throughout the entire process, from purchase, to delivery, to installation and activation. Our team of tech experts will ensure a smooth installation process for you. <br>
+            <br><b>8. Why are your prices so low? </b>
+            <br>We have a volume account that can procure licenses at discounted rates, licenses that we can then offer to our customers at greater cost savings. <br>
+            <br><b>9. Can I get a discount on bulk orders? </b>
+            <br>Absolutely. Just contact us via an email or send us a message on Facebook. We’ll get it arranged. <br>
+            <br><b>10. Why buy from us? </b>
+            <br>Three reasons: fast, cheap and easy. Our secure website (powered by PayPal) ensures that our customers get the best shopping experience where software products are delivered at low prices. We have a team of experts that provides technical support and we have an easy-refund policy.&nbsp;
+          </span>
+          <br>
+        </p>
+        <h1 class="u-text u-text-default u-text-white u-text-2">FAQs</h1>
+      </div>
+    </section>
+    <section class="u-clearfix u-gradient u-section-3" src="" id="sec-6b19">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width-xs u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-center-sm u-align-center-xs u-align-left-lg u-align-left-md u-align-left-xl u-container-style u-layout-cell u-left-cell u-opacity u-opacity-65 u-size-39-md u-size-39-sm u-size-39-xs u-size-45-lg u-size-45-xl u-white u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-text u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xl u-text-1">Buy Your Product Key Today.</h2>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-opacity u-opacity-65 u-right-cell u-size-15-lg u-size-15-xl u-size-21-md u-size-21-sm u-size-21-xs u-white u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xl u-container-layout-2">
+                  <a href="https://cheapkeys.online/shop/" class="u-border-0 u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-btn-1">Buy Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-87e3"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> © 2022. 2032 Cecil Street, Chicago, IL 60606&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse">
+            <a class="u-button-style u-nav-link" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-62b6"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="svg-62b6" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="CheapKeys.html">CheapKeys</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms.html">Terms</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy.html">Privacy</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="CheapKeys.html">CheapKeys</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms.html">Terms</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy.html">Privacy</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/templates" target="_blank">
+        <span>Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/static-site-generator" target="_blank">
+        <span>Static Site Generator</span>
+      </a>. 
+    </section>
+  </body>
+</html>
